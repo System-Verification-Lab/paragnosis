58796332c5ac77c9c926eb68ad811c28bc733144
gdal @ clevo <gisodal@gmail.com>
2022-11-06 23:13:43 +0100

    created an extensive readme

--- README.md
+++ README.md
@@ -1,17 +1,145 @@
 # ParaGnosis
 
-## Installation (Unbuntu 18.04+)
+ParaGnosis is a weighted model counting toolset. Its implementation is based on [[1,2,3,4]](#4). We have also added a significant number of Bayesian networks to play with (under *./data/net*)
+
+The toolset consists of the following:
+
+  * `bn-to-cnf`: a c++ tool to create Conjunctive Normal Form (CNF) encodings from a Bayesian network.
+  * `bnc`: a c/c++ **B**ayesian **N**etwork **C**ompiler for multiple target representations.
+  * `bnmc`: a  c++ **B**ayesian **N**etwork **M**odel **C**ounter.
+  * `pg`: a **P**ara**G**nosis user friendly jjinterface to the tools above, written in Python.
+
+The currently supported target languages are: 
+
+  * Weighted Positive Binary Decision Diagrams (WPBDD)
+  * Weighted Positive Multi-Valued Decision Diagrams (WPMDD)
+  * Tree-driven Weighted Positive Multi-valued Decision Diagrams (TD-WPMDD)
+
+## Installation (Ubuntu 18.04+)
 
 Install requirements with `apt`:
 
     > sudo apt-get install -y libboost-all-dev python3 python-setuptools make cmake gcc g++ libgmp-dev libgsl-dev libreadline-dev make cmake
-Install latest pip:
+
+Install latest pip (the python package installer):
 
     > sudo python3 -m pip install --upgrade pip
 
-To build, type:
+To build all tools in the toolset, type:
 
     > make
 
-Binaries will be installed in the '<path/to/source>/bin' folder.
+Binaries will be installed in the `<path/to/source>/bin` directory, and the `pg` script will be available system wide. 
 
+## Configure `pg`
+
+In order to let the `pg` script know where the toolset is located, we need to run all `pg` commands with `pg --source-dir=<path/to/source> ...`, or add to following to its configuration file `~/.pgrc`:
+
+    location = <path/to/source>
+
+To test if the installation is successful, we can give the following a try. Open a terminal at any location and type:
+
+    > pg encode asia
+    or:
+    > pg --source-dir=<path/to/source> encode asia
+
+This should produce encoding statistics for the *asia* network.
+
+## Examples
+
+For the following examples, we assume `location` has been set in `~/pgrc`. All available commands can be found through `pg --help`.
+
+### Encoding and available networks
+
+#### Show a list of available Bayesian networks
+
+    > pg --list
+
+        3nt
+        4sp
+        6hj
+        6nt
+        aggregate
+        alarm
+        ...
+
+Any of the shown names can be used as input for the `pg` script.
+
+#### Show encoding statistics
+
+    > pg encode asia
+
+        ...
+
+        Variables       : 8
+        Probabilities   : 36
+        Deterministic   : 8
+        Unsatisfiable   : 4
+        Literals        : 16
+        Clauses         : 52
+        Literal/clauses : 2.23
+        Clause sizes    : 1-3
+
+
+### Compiling a network
+
+#### Compile asia to a TD-WPMDD
+
+    > pg compile asia
+
+#### Compile asia to a WPBDD
+
+    > pg compile asia --method wpbdd
+
+#### Compare compilation between WPBDD, a WPMDD, and a TD-WPMDD
+
+    > pg compile asia --method wpbdd mg tdmg
+
+#### Compile a local Hugin file to 
+
+    > pg compile <path/to/file>/asia.net
+
+
+#### Directly visualize (with evince) the WPBDD
+
+    > pg compile asia --method wpbdd --dot
+
+#### Directly visualize the TD-WPMDD
+
+    > pg compile asia --method tdmg --dot
+
+### Perform Inference
+
+#### Run every possible marginalization on a network using TD-WPMDD (press ctrl-c to stop)
+
+    > pg inference asia
+
+#### Compute all posteriors for evidence `bronc = yes`, and `smoke = yes`.
+
+    > pg inference asia --evidence='bronc=yes,smoke=yes'
+
+#### Compute all posteriors of `lung` and `xray` for evidence `bronc = yes`, and `smoke = yes`
+
+    > pg inference asia --evidence='bronc=yes,smoke=yes' --posteriors=lung,xray
+
+        ...
+
+        lung=yes: 0.100000
+        lung=no: 0.900000
+        xray=yes: 0.151705
+        xray=no: 0.848295
+
+
+## References
+
+<a id="1">[1]</a>
+G.H. Dal, A.W. Laarman, A. Hommerso and P.J.F. Lucas, ”*A Compositional Approach to Probabilistic Knowledge Compilation*”, in International Journal of Approximate Reasoning, vol 138:38-66, 2021.
+
+<a id="2">[2]</a>
+G.H. Dal, A.W. Laarman and P.J.F. Lucas, ”*Parallel Probabilistic Inference by Weighted Model Counting*”, in Proceeding of the International Conference on Probabilistic Graphical Models, PMLR, vol 72:97-108, 2018.
+
+<a id="3">[3]</a>
+G.H. Dal, S. Michels and P.J.F. Lucas, ”*Reducing the Cost of Probabilistic Knowledge Compilation*”, in Proceedings of Machine Learning Research, volume 73, pages 41-152, 2017.
+
+<a id="4">[4]</a>
+G.H. Dal and P.J.F. Lucas, ”*Weighted Positive Binary Decision Diagrams for Exact Probabilistic Inference*”, in Journal of Approximate Reasoning, volume 90, pages 411-432, 2017.
--- src/paragnosis/paragnosis/main.py
+++ src/paragnosis/paragnosis/main.py
@@ -47,11 +47,10 @@ def cli():
 
     parser.add_argument('--help', '-h',    action='help',                             help='Show this help message and exit')
     parser.add_argument('--version',       action='store_true',                       help='Print version and exit')
-    #parser.add_argument('--dry-run',    action='store_true',                       help='Print the commands instead of running them')
     parser.add_argument('--verbose', '-v', dest='verbose',      action='store_true',  help='Print debug info')
     parser.add_argument('--list',          action='store_true',                       help='Print list of available Bayesian networks')
-    parser.add_argument('--output-dir',    dest='output_dir',   metavar="#DIRECTORY", help='Set the output directory (current: {})'.format(settings.output_dir), type=str)
-    parser.add_argument('--source',        dest='location',     metavar="#DIRECTORY", help='Set the source directory of the Paragnosis toolset (current: {})'.format(settings.get("location", "unknown")), type=str, default=settings.get("location"))
+    parser.add_argument('--output-dir',    dest='output_dir',   metavar="DIRECTORY",  help='Set the output directory (current: {})'.format(settings.output_dir), type=str)
+    parser.add_argument('--source-dir',    dest='location',     metavar="DIRECTORY",  help='Set the source directory of the Paragnosis toolset (current: {})'.format(settings.get("location", "unknown")), type=str, default=settings.get("location"))
 
     bdd_options = ["wpbdd","mg","tdmg"] # "parallel-wpbdd","pwpbdd","parallel-pwpbdd",
     bdd_default = ["tdmg"]
@@ -68,22 +67,22 @@ def cli():
     compilation = subparsers.add_parser('compile', help='Compile Bayesian networks')
     compilation.add_argument(                dest='network',                                         metavar="NETWORK",     type=str,             help="Provide a hugin file or a Bayesian network name provided by ./pg --list")
     compilation.add_argument('--method',     dest='bdds',       required=False, default=bdd_default, metavar='BDD',         nargs='+',            help='Type of BDD. Options are ' + ', '.join(bdd_options), choices=bdd_options)
-    #compilation.add_argument('--partitions', dest='partitions', required=False, default=2,           metavar='#PARTITIONS', type=int,             help='Set number of partitions')
     compilation.add_argument('--overwrite',  dest='overwrite',  required=False,                                             action='store_true',  help='Overwrite ordering, partitioning, etc.')
     compilation.add_argument('--repeat',     dest='repeat',     required=False, default=1,                                  type=int,             help='Set number of compilation repeats')
     compilation.add_argument('--dot',        dest='dot',        action='store_true',                                                              help='Output the created compiled representation as a dot file')
-    #compilation.add_argument('--cores',      dest='cores',      required=False,                      metavar="#CORES",      type=int,  nargs='+', help='Number of cores to use during parallel execution', )
+    #compilation.add_argument('--cores',     dest='cores',      required=False,                      metavar="#CORES",      type=int,  nargs='+', help='Number of cores to use during parallel execution', )
+    #compilation.add_argument('--partitions',dest='partitions', required=False, default=2,           metavar='#PARTITIONS', type=int,             help='Set number of partitions')
 
     # inference parser
     inference = subparsers.add_parser('inference', help='Perform inference')
     inference.add_argument(                dest='network',                                         type=str,  metavar="NETWORK",                help="Provide a hugin file or a Bayesian network name provided by ./pg --list")
-    inference.add_argument('--evidence',   dest='evidence',   required=False, default=None,        type=str,  metavar='#EVIDENCE',              help='Provide evidence in the form \'var=value[, var2=value2][, var3=value3]\'')
-    inference.add_argument('--posteriors', dest='posteriors', required=False, default=None,        type=str,  metavar='#VARIABLES',             help='Provide variables in the form \'var[, var2][, var3]\'', nargs='?')
+    inference.add_argument('--evidence',   dest='evidence',   required=False, default=None,        type=str,  metavar='EVIDENCE',              help='Provide evidence in the form \'var=value[, var2=value2][, var3=value3]\'')
+    inference.add_argument('--posteriors', dest='posteriors', required=False, default=None,        type=str,  metavar='VARIABLES',             help='Provide variables in the form \'var[, var2][, var3]\'', nargs='?')
     inference.add_argument('--verify',     dest='verify',     required=False,                                 action='store_true',              help='Verify inference answers')
     inference.add_argument('--compare',    dest='compare',    required=False, default="",          type=str,  metavar="#NROBSERVED", nargs='?', help='Limit number of observerd variables during inference', const="")
-    inference.add_argument('--cases',      dest='cases',      required=False,                      type=str,  metavar='#FILE',                  help='Provide a file with inference test cases\'')
+    inference.add_argument('--cases',      dest='cases',      required=False,                      type=str,  metavar='FILE',                  help='Provide a file with inference test cases\'')
     inference.add_argument('--method',     dest='bdds',       required=False, default=bdd_default,            metavar='BDD',         nargs='+', help='Type of BDD. Options are ' + ', '.join(bdd_options), choices=bdd_options)
-    #inference.add_argument('--cores',      dest='cores',      required=False,                      type=int,  metavar="#CORES",      nargs='+', help='Number of cores to use during parallel execution', )
+    #inference.add_argument('--cores',     dest='cores',      required=False,                      type=int,  metavar="#CORES",      nargs='+', help='Number of cores to use during parallel execution', )
 
     if len(sys.argv)==1:
         parser.print_help()
